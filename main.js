1f782143a5d86b6a9643d4088aea20cb3c530d4d
penguinboots <sabrina.ynw@gmail.com>
2023-12-21 09:06:11 -0800

    add mode toggle

--- main.js
+++ main.js
@@ -3,13 +3,34 @@ window.onload = function() {
   const gameScreen = document.getElementById("game-screen");
 
   const vsPCBtn = document.getElementById("vs-pc");
-  const vs2PBtn = document.getElementById("2p-vs")
+  const vs2PBtn = document.getElementById("2p-vs");
+
+  const pauseBtn = document.getElementById("pause");
+  const quitBtn = document.getElementById("quit");
+  const restartBtn = document.getElementById("restart");
 
   vsPCBtn.addEventListener('click', function() {
     console.log("1P")
+    toggleMode();
   })
 
   vs2PBtn.addEventListener('click', function() {
     console.log("2P")
+    toggleMode();
   })
+
+  quitBtn.addEventListener('click',function() {
+    toggleMode();
+  })
+
+  function toggleMode() {
+    if (startScreen.style.display !== "none") {
+      startScreen.style.display = "none";
+      gameScreen.style.display = "block";
+    } else {
+      startScreen.style.display = "flex";
+      gameScreen.style.display = "none";
+    }
+  }
 }
+
